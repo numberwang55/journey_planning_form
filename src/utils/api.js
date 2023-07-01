f27028e2409b41225fa0604da5c9c172ebe7cede
numberwang55 <cban0901@hotmail.com>
2023-07-01 22:59:09 +0100

    api call using axios

---
+++ src/utils/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const apiUrl = axios.create({
+    baseURL: "https://media.carecontrolsystems.co.uk"
+})
+
+const getJourney = (route, format = "miles", travelMode = "driving", trafficmodal= "best_guess") => {
+    return apiUrl.get("/Travel/JourneyPlan.aspx", {
+        params: {
+            route: route.toString(),
+            format: format,
+            travelMode: travelMode,
+            trafficmodal: trafficmodal
+        }
+    }).then(({data} )=> data)
+}
+
+getJourney(["ex203rx, sg86ed"]).then(res => console.log(res))
